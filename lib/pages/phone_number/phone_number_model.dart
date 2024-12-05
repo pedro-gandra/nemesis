7f9e74104318266f634d492cdf4fe235194b727b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-05 20:55:09 +0000

    custom flag for phone number, portuguese translation

--- lib/pages/phone_number/phone_number_model.dart
+++ lib/pages/phone_number/phone_number_model.dart
@@ -5,19 +5,43 @@ import 'package:flutter/material.dart';
 class PhoneNumberModel extends FlutterFlowModel<PhoneNumberWidget> {
   ///  State fields for stateful widgets in this page.
 
-  final formKey = GlobalKey<FormState>();
-  // State field(s) for TextField widget.
-  FocusNode? textFieldFocusNode;
-  TextEditingController? textController;
-  String? Function(BuildContext, String?)? textControllerValidator;
-  String? _textControllerValidator(BuildContext context, String? val) {
+  final formKey2 = GlobalKey<FormState>();
+  final formKey1 = GlobalKey<FormState>();
+  // State field(s) for nocode widget.
+  FocusNode? nocodeFocusNode;
+  TextEditingController? nocodeTextController;
+  String? Function(BuildContext, String?)? nocodeTextControllerValidator;
+  String? _nocodeTextControllerValidator(BuildContext context, String? val) {
     if (val == null || val.isEmpty) {
-      return 'Field is required';
+      return FFLocalizations.of(context).getText(
+        'h3aji55u' /* Field is required */,
+      );
+    }
+
+    if (!RegExp('^[\\d]{8,15}([-\\s\\(\\)]?[\\d]{1,4})*\$').hasMatch(val)) {
+      return FFLocalizations.of(context).getText(
+        'ui7bq7aq' /* Use a valid phone number */,
+      );
+    }
+    return null;
+  }
+
+  // State field(s) for withcode widget.
+  FocusNode? withcodeFocusNode;
+  TextEditingController? withcodeTextController;
+  String? Function(BuildContext, String?)? withcodeTextControllerValidator;
+  String? _withcodeTextControllerValidator(BuildContext context, String? val) {
+    if (val == null || val.isEmpty) {
+      return FFLocalizations.of(context).getText(
+        'uhaiibob' /* Field is required */,
+      );
     }
 
     if (!RegExp('^\\+[1-9]\\d{1,14}(\\s*\\(?\\d+\\)?[\\s-]*)*\$')
         .hasMatch(val)) {
-      return 'Use a valid phone number';
+      return FFLocalizations.of(context).getText(
+        'xrcyc03a' /* Use a valid phone number */,
+      );
     }
     return null;
   }
@@ -29,12 +53,16 @@ class PhoneNumberModel extends FlutterFlowModel<PhoneNumberWidget> {
 
   @override
   void initState(BuildContext context) {
-    textControllerValidator = _textControllerValidator;
+    nocodeTextControllerValidator = _nocodeTextControllerValidator;
+    withcodeTextControllerValidator = _withcodeTextControllerValidator;
   }
 
   @override
   void dispose() {
-    textFieldFocusNode?.dispose();
-    textController?.dispose();
+    nocodeFocusNode?.dispose();
+    nocodeTextController?.dispose();
+
+    withcodeFocusNode?.dispose();
+    withcodeTextController?.dispose();
   }
 }
